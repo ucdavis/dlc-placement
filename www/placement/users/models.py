0b552059e405f7f92c3366d30afd817c0ca68495
Edgar Duran-Perez <eduranperez@ucdavis.edu>
2021-07-02 15:57:58 -0700

    model changes

--- www/placement/users/models.py
+++ www/placement/users/models.py
@@ -7,9 +7,9 @@ from django.db import models
 class Users(models.Model):
     id = models.AutoField(primary_key=True, unique=True) # AutoField?
     cas_user = models.CharField(max_length=255, unique=True)
-    first_name = models.CharField(max_length=50, blank=True)
-    last_name = models.CharField(max_length=50, blank=True)
-    department = models.CharField(max_length=255, blank=True)
+    first_name = models.CharField(max_length=50, blank=True, null=True)
+    last_name = models.CharField(max_length=50, blank=True, null=True)
+    department = models.CharField(max_length=255, blank=True, null=True)
     email = models.CharField(max_length=254, blank = True)
     advisor = models.BooleanField(default = False)
     tester = models.BooleanField(default = False)
