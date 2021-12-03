644bd5e95825daef8f73fa3a389c4f263ddfb794
Edgar Duran-Perez <eduranperez@ucdavis.edu>
2021-12-02 16:02:37 -0800

    add support to search via email

--- www/placement/scoresheet/urls.py
+++ www/placement/scoresheet/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
      url(r'^create/$', views.scoresheet_create, name="create"),
      url(r'^get_levels/(?P<language_id>\d+)/$', views.get_levels, name='get_levels'),
      url(r'^GetStudentBanner/(?P<sid>\d+)/(?P<formatted>\w+)/$', views.GetStudentBanner, name='get_student'),
+     url(r'^GetStudentInfoFromEmail/(?P<email>\w+|[\w.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4})/$', views.GetStudentInfoFromEmail, name='get_student_info_from_email'),
      url(r'^(?P<id>\d+)/detail/$', views.scoresheet_detail, name="detail"),
      url(r'^(?P<id>\d+)/edit/$', views.scoresheet_update, name="edit"),
      url(r'^(?P<id>\d+)/delete$', views.scoresheet_delete, name="delete"),
